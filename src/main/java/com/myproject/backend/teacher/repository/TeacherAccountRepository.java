75a524ade23d986909b8829c91fbeb569b7eb99d
rowel <razonabler23@gmail.com>
2025-05-08 07:17:54 -0700

    changes side effect

--- src/main/java/com/myproject/backend/teacher/repository/TeacherAccountRepository.java
+++ src/main/java/com/myproject/backend/teacher/repository/TeacherAccountRepository.java
@@ -10,6 +10,9 @@ import com.myproject.backend.teacher.entity.TeacherAccountEntity;
 @Repository
 public interface TeacherAccountRepository extends JpaRepository<TeacherAccountEntity, Integer> {
 	
-	Optional <TeacherAccountEntity> findByEmail(String email);
-
+	//Optional<TeacherAccountEntity> findByEmail(String email);
+    Optional<TeacherAccountEntity> findByUsername(String username);
+    
+    boolean existsByUsername(String username);
+    
 }
