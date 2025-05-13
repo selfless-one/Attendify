576035f1667341a5c04188b3fd92a4eaf0be3259
rowel <razonabler23@gmail.com>
2025-05-13 11:21:55 -0700

    fix default route issue

---
+++ src/main/java/DefaultRouteView.java
@@ -0,0 +1,18 @@
+import com.myproject.student.ui.view.StudentLoginView;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class DefaultRouteView extends VerticalLayout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DefaultRouteView() {
+		UI.getCurrent().navigate(StudentLoginView.class);
+	}
+
+}
